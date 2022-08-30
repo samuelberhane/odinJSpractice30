a8d152ed398cb97a5569066f602afc6b817891fa
Samuel Brhane <61617535+samuelberhane@users.noreply.github.com>
2022-08-30 12:20:33 +0300

    day 6 js practice

--- devToolsTricks/script.js
+++
@@ -1,9 +0,0 @@
-function change() {
-  let h1 = document.querySelector("h1");
-  h1.style.cssText = "color: green; font-size: 50px";
-}
-
-function reset() {
-  let h1 = document.querySelector("h1");
-  h1.style.cssText = "color: black; font-size: 30px";
-}
